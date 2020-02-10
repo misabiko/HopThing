f839fd53f999fb8da9e8c7215b4cb75bdc26367c
misabiko <misabiko@gmail.com>
2020-02-10 15:52:41 -0500

    Check if viewport still exist on gameover

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -7,7 +7,8 @@ onready var lower_rect = $".."/LowerRect
 
 func game_over():
 	visible = true
-	margin_left = get_viewport().size.x * 2 + rect_size.x + 100
+	if get_viewport() != null:
+		margin_left = get_viewport().size.x * 2 + rect_size.x + 100
 	
 	tween.interpolate_property(self, "margin_left", margin_left, 0, 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.5)
 	tween.interpolate_property($".."/UpperRect, "margin_bottom", 0, 375, 1.25, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.5)
